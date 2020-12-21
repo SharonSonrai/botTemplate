dadf7f3be282fe9bb24c319dcb34d536edbb1cb3
Sharon Cohen <sharon.cohen@Sonrai-SharonCohen.local>
2020-12-21 15:40:33 -0400

    feat: delete user

---
+++ bot1/bot.py
@@ -0,0 +1,18 @@
+import logging
+import sonrai.platform.aws.arn
+
+def run(ctx):
+
+    iam_client = ctx.get_client().get('iam')
+
+    # Get policy arn
+    resource_arn = sonrai.platform.aws.arn.parse(ctx.resource_id)
+    policy_arn = resource_arn \
+        .assert_service("iam") \
+        .assert_type("policy") \
+        .resource
+
+    # https://docs.aws.amazon.com/cli/latest/reference/iam/delete-policy.html
+
+    logging.info('deleting policy: {}'.format(ctx.policy_arn))
+    iam_client.delete_policy(PolicyArn=policy_arn)
