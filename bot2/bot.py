abd78de8837a3eb27acc7bdf15ef0cfa46810d4c
Sharon Cohen <sharon.cohen@Sonrai-SharonCohen.local>
2020-12-22 10:09:13 -0400

    fix:bot2-name-capital

--- bot2/bot.py
+++ bot2/bot.py
@@ -4,13 +4,14 @@ def run(ctx):
 
     s3 = ctx.get_client().get('s3')
 
-    # Get role name
+    # Get reource id name
     resource_id = ctx.resource_id
 
+    #Get bucket name
     splited_resource = resource_id.split(":::")
     bucket_name = splited_resource[1]
-    logging.info('Creating bucket: {}-AuditData'.format(bucket_name))
-    s3.create_bucket(Bucket="{}-AuditData".format(bucket_name))
+    logging.info('Creating bucket: {}-auditdata'.format(bucket_name))
+    s3.create_bucket(Bucket="{}-auditdata".format(bucket_name))
 
-    logging.info('Logging bucket: {}-AuditData'.format(bucket_name))
-    s3.BucketLogging(Bucket="{}-AuditData".format(bucket_name))
+    logging.info('Logging bucket: {}-auditdata'.format(bucket_name))
+    s3.BucketLogging(Bucket="{}-auditdata".format(bucket_name))
