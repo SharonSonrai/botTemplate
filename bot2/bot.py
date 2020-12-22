580eb8ea421d5a72413b0876343549560a9b24cd
Sharon Cohen <sharon.cohen@Sonrai-SharonCohen.local>
2020-12-22 09:52:46 -0400

    fix:bot2-name

--- bot2/bot.py
+++ bot2/bot.py
@@ -7,8 +7,10 @@ def run(ctx):
     # Get role name
     resource_id = ctx.resource_id
 
-    logging.info('Creating bucket: {}-AuditData'.format(resource_id))
-    s3.create_bucket(Bucket="{}-AuditData".format(resource_id))
+    splited_resource = resource_id.split(":::")
+    bucket_name = splited_resource[1]
+    logging.info('Creating bucket: {}-AuditData'.format(bucket_name))
+    s3.create_bucket(Bucket="{}-AuditData".format(bucket_name))
 
-    logging.info('Logging bucket: {}-AuditData'.format(resource_id))
-    s3.BucketLogging(Bucket="{}-AuditData".format(resource_id))
+    logging.info('Logging bucket: {}-AuditData'.format(bucket_name))
+    s3.BucketLogging(Bucket="{}-AuditData".format(bucket_name))
